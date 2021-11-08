e8783f51ffbef73a35f5ac2d345b993958f07e36
Ilya Bylich <ibylich@gmail.com>
2021-11-09 01:16:54 +0300

    split impls into separate files

---
+++ src/tagged_pointer.rs
@@ -0,0 +1,67 @@
+use crate::TaggedPointerValue;
+
+pub struct TaggedPointer<const BITS: u8> {
+    ptr: usize,
+}
+
+impl<const BITS: u8> TaggedPointer<BITS> {
+    const VALUE_BITS: u8 = 64 - BITS;
+
+    fn new_from_usize<T, const MASK: usize>(value: usize) -> Self
+    where
+        T: TaggedPointerValue<BITS>,
+    {
+        debug_assert!(MASK < (1_usize << BITS));
+        let tag_mask = MASK << Self::VALUE_BITS;
+        let ptr = value | tag_mask;
+        Self { ptr }
+    }
+
+    pub fn new<T, const MASK: usize>(value: T) -> Self
+    where
+        T: TaggedPointerValue<BITS> + std::fmt::Debug,
+    {
+        Self::new_from_usize::<T, MASK>(T::as_untagged_ptr(value))
+    }
+
+    pub fn is<const MASK: usize>(&self) -> bool {
+        self.tag() == MASK
+    }
+
+    pub fn unwrap<T>(mut self) -> T
+    where
+        T: TaggedPointerValue<BITS>,
+    {
+        let untagged_ptr = self.without_tag();
+        self.ptr = 0;
+        T::unwrap(untagged_ptr)
+    }
+
+    pub fn borrow_value<T, U>(&self) -> &U
+    where
+        T: TaggedPointerValue<BITS> + std::borrow::Borrow<U>,
+    {
+        T::borrow_value(self)
+    }
+
+    pub fn tag(&self) -> usize {
+        self.ptr >> Self::VALUE_BITS
+    }
+
+    fn tag_mask(&self) -> usize {
+        self.tag() << Self::VALUE_BITS
+    }
+
+    pub fn without_tag(&self) -> usize {
+        self.ptr ^ self.tag_mask()
+    }
+
+    pub fn drop_as<T>(&mut self)
+    where
+        T: TaggedPointerValue<BITS>,
+    {
+        let untagged_ptr = self.without_tag();
+        self.ptr = 0;
+        drop(T::unwrap(untagged_ptr));
+    }
+}
---
+++ src/tagged_pointer_value.rs
@@ -0,0 +1,50 @@
+use crate::TaggedPointer;
+
+pub trait TaggedPointerValue<const BITS: u8> {
+    fn as_untagged_ptr(this: Self) -> usize
+    where
+        Self: Sized + std::fmt::Debug,
+    {
+        let mut untagged_ptr = 0;
+        let dst: *mut u8 = &mut untagged_ptr as *mut usize as *mut u8;
+        let src: *const u8 = &this as *const Self as *const u8;
+        unsafe { std::ptr::copy(src, dst, std::mem::size_of::<Self>()) }
+        std::mem::forget(this);
+        untagged_ptr
+    }
+    fn from_untagged_ptr(untagged_ptr: usize) -> Self
+    where
+        Self: Sized,
+    {
+        let mut this = std::mem::MaybeUninit::<Self>::uninit();
+        let dst = this.as_mut_ptr() as *mut u8;
+        let src = &untagged_ptr as *const usize as *const u8;
+        unsafe { std::ptr::copy(src, dst, std::mem::size_of::<Self>()) };
+        unsafe { this.assume_init() }
+    }
+
+    fn unwrap(untagged_ptr: usize) -> Self
+    where
+        Self: Sized,
+    {
+        Self::from_untagged_ptr(untagged_ptr)
+    }
+
+    fn drop_inner(untagged_ptr: usize)
+    where
+        Self: Sized,
+    {
+        drop(Self::unwrap(untagged_ptr))
+    }
+
+    fn borrow_value<U>(tagged_ptr: &TaggedPointer<BITS>) -> &U
+    where
+        Self: std::borrow::Borrow<U> + Sized,
+    {
+        let untagged_ptr = tagged_ptr.without_tag();
+        let this: Self = unsafe { std::mem::transmute_copy(&untagged_ptr) };
+        let borrowed: &U = unsafe { std::mem::transmute(this.borrow()) };
+        std::mem::forget(this);
+        borrowed
+    }
+}
